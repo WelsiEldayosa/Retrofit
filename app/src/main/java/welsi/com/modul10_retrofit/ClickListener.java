2fef7e37d52660b2cf84b770fcb5240dbc1ec958
WelsiEldayosa <39894528+WelsiEldayosa@users.noreply.github.com>
2018-11-19 02:05:31 +0700

    retro

---
+++ app/src/main/java/welsi/com/modul10_retrofit/ClickListener.java
@@ -0,0 +1,8 @@
+package welsi.com.modul10_retrofit;
+
+import android.view.View;
+
+public interface ClickListener {
+    void onClick(View view, int position);
+    void onLongClick(View view, int position);
+}
